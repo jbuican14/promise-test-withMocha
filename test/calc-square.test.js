b8eaeb3aa23ebc9dda45929b67badfa14737573d
Bew <Bew@sysops-MBP.broadband>
2020-11-29 14:03:26 +0000

    Multiple expect test case added and include reject promise return

--- test/calc-square.test.js
+++ test/calc-square.test.js
@@ -8,8 +8,11 @@ chai.use(chaiAsPromised);
 describe('calculateSquare', function () {
   this.timeout(4000);
 
-  it('should resolve with number 4 if passed number 2', function (done) {
-    expect(calculateSquare(2)).to.eventually.be.equal(4).notify(done);
+  it('should resolve with number 4 if passed number 2', function () {
+    return calculateSquare(2).then((result) => {
+      expect(result).to.be.above(3);
+      expect(result).to.be.equal(4);
+    });
   });
 
   it('should become fulfilled when passed number 2', function (done) {
@@ -18,5 +21,13 @@ describe('calculateSquare', function () {
 
   it('should become rejected if pass a string instead of a number', function (done) {
     expect(calculateSquare('three')).to.be.rejected.notify(done);
+  });
+
+  //alternatively
+  it('should return a rejected promise if a string is passed', function () {
+    return calculateSquare('str').catch((reason) => {
+      expect(reason).to.not.equal(null);
+      expect(reason.message).to.equal('Argument of type number is expected');
+    });
   });
 });
